671c11af62fa380c11e53c2294852c448b3dd64f
sunilmeena26 <sunilmeena641085@gmail.com>
2025-01-07 16:32:50 +0530

    Add Addition Code

---
+++ Add.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class
+{
+ public static void main(String []args)
+ {
+  System.out.println("Enter A Number : ");
+  Scanner sc=new Scanner(System.in);
+  int num1=sc.nextInt();
+  int num2=sc.nextInt();
+  int sum=num1+num2;
+  System.out.println("Sum of two number is : "+sum);
+ }
+}
